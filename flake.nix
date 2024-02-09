544e18892de1b51cd669dc0446f3a709f65acfc8
Mitchell Hanberg <mitch@mitchellhanberg.com>
2024-02-09 10:54:34 -0500

    fixup! ci: introduce CI

--- flake.nix
+++ flake.nix
@@ -2,36 +2,51 @@
   description = "spitfire";
 
   inputs = {
-    beam-flakes = {
-      url = "github:shanesveller/nix-beam-flakes";
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    devshell.url = "github:numtide/devshell";
   };
 
   outputs = inputs @ {
-    beam-flakes,
     flake-parts,
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [beam-flakes.flakeModule];
+      imports = [
+        inputs.devshell.flakeModule
+      ];
 
       systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
 
-      perSystem = _: {
-        beamWorkspace = {
-          enable = true;
-          devShell = {
-            languageServers.elixir = false;
-            languageServers.erlang = false;
-          };
-          versions = {
-            elixir = "1.16.1";
-            erlang = "26.2.1";
-          };
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              elixir = prev.elixir.overrideAttrs (old: {
+                src = prev.fetchFromGitHub {
+                  owner = "elixir-lang";
+                  repo = "elixir";
+                  rev = "52eaf1456182d5d6cce22a4f5c3f6ec9f4dcbfd9";
+                  hash = "sha256-fOsV+jVIzsa38hQDvAjhUqee36nt8kG6AOpOQJnSZ74=";
+                };
+              });
+            })
+          ];
+          config = {};
+        };
+        devshells.default = {
+          packages = [
+            pkgs.erlang_26
+            pkgs.elixir
+          ];
         };
       };
     };
