5c1371cd485289160d5c7c174da5a24a59aca38b
Mitchell Hanberg <mitch@mitchellhanberg.com>
2024-02-09 13:21:19 -0500

    ci: introduce CI (#6)

--- flake.nix
+++ flake.nix
@@ -2,11 +2,8 @@
   description = "spitfire";
 
   inputs = {
-    beam-flakes = {
-      url = "github:shanesveller/nix-beam-flakes";
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    beam-flakes.url = "github:shanesveller/nix-beam-flakes";
+    beam-flakes.inputs.flake-parts.follows = "flake-parts";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
@@ -21,18 +18,43 @@
 
       systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
 
-      perSystem = _: {
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
         beamWorkspace = {
           enable = true;
-          devShell = {
-            languageServers.elixir = false;
-            languageServers.erlang = false;
+          devShell.languageServers.elixir = false;
+          devShell.languageServers.erlang = false;
+          flakePackages = true;
+          pkgSet = let
+            # Provide your desired OTP version here
+            # erlang = pkgs.beam.interpreters.erlang_26;
+            erlang = beam-flakes.lib.mkErlang pkgs "26.2.1" beam-flakes.lib.versions.erlang."26.2.1";
+          in
+            (pkgs.beam.packagesWith erlang).extend (final: prev: {
+              elixir_1_17 = prev.elixir_1_16.override {
+                rev = "52eaf1456182d5d6cce22a4f5c3f6ec9f4dcbfd9";
+                # You can discover this using Trust On First Use by filling in `lib.fakeHash`
+                sha256 = "sha256-fOsV+jVIzsa38hQDvAjhUqee36nt8kG6AOpOQJnSZ74=";
+                version = "1.17.0-dev";
+              };
+
+              elixir = final.elixir_1_17;
+              # This will get upstreamed into nix-beam-flakes at some point
+              rebar = prev.rebar.overrideAttrs (_old: {doCheck = false;});
+              rebar3 = prev.rebar3.overrideAttrs (_old: {doCheck = false;});
+            });
+          packages = {
+            inherit (config.beamWorkspace.pkgSet) elixir erlang elixir-ls erlang-ls;
           };
           versions = {
-            elixir = "1.16.1";
-            erlang = "26.2.1";
+            elixir = null;
+            erlang = null;
           };
         };
       };
     };
 }
+
