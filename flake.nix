b3f87f5d5f1bf62a831dc0d51d8f18618db26d57
Mitchell Hanberg <mitch@mitchellhanberg.com>
2024-06-13 09:02:55 -0400

    feat: environment querying

--- flake.nix
+++ flake.nix
@@ -1,57 +1,46 @@
 {
-  description = "spitfire";
+  description = "Spitfire parser for Elixir";
 
   inputs = {
-    beam-flakes.url = "github:shanesveller/nix-beam-flakes";
-    beam-flakes.inputs.flake-parts.follows = "flake-parts";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs @ {
-    beam-flakes,
-    flake-parts,
-    ...
-  }:
+  outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [beam-flakes.flakeModule];
-
-      systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
-
+      imports = [];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {
         config,
+        self',
+        inputs',
         pkgs,
+        system,
         ...
-      }: {
-        beamWorkspace = {
-          enable = true;
-          devShell.languageServers.elixir = false;
-          devShell.languageServers.erlang = false;
-          flakePackages = true;
-          pkgSet = let
-            # Provide your desired OTP version here
-            # erlang = pkgs.beam.interpreters.erlang_26;
-            erlang = beam-flakes.lib.mkErlang pkgs "26.2.1" beam-flakes.lib.versions.erlang."26.2.1";
-          in
-            (pkgs.beam.packagesWith erlang).extend (final: prev: {
-              elixir_1_17 = prev.elixir_1_16.override {
-                rev = "e3b6a91b173f7e836401a6a75c3906c26bd7fd39";
-                # You can discover this using Trust On First Use by filling in `lib.fakeHash`
-                sha256 = "sha256-RK0aMW7pz7kQtK9XXN1wVCBxKOJKdQD7I/53V8rWD04=";
-                version = "1.17.0-dev";
-              };
-
-              elixir = final.elixir_1_17;
-              # This will get upstreamed into nix-beam-flakes at some point
-              rebar = prev.rebar.overrideAttrs (_old: {doCheck = false;});
-              rebar3 = prev.rebar3.overrideAttrs (_old: {doCheck = false;});
-            });
-          packages = {
-            inherit (config.beamWorkspace.pkgSet) elixir erlang elixir-ls erlang-ls;
+      }: let
+        beamPackages = pkgs.beam_minimal.packages.erlang_27;
+        otp = (pkgs.beam.packagesWith beamPackages.erlang).extend (final: prev: {
+          elixir_1_17 = prev.elixir_1_16.override {
+            rev = "v1.17.0";
+            # You can discover this using Trust On First Use by filling in `lib.fakeHash`
+            sha256 = "sha256-RBylCfD+aCsvCqWUIvqXi3izNqqQoNfQNnQiZxz0Igg=";
+            version = "1.17.0";
           };
-          versions = {
-            elixir = null;
-            erlang = null;
+
+          elixir = final.elixir_1_17;
+          # This will get upstreamed into nix-beam-flakes at some point
+          rebar = prev.rebar.overrideAttrs (_old: {doCheck = false;});
+          rebar3 = prev.rebar3.overrideAttrs (_old: {doCheck = false;});
+        });
+        elixir = otp.elixir;
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            # The Nix packages provided in the environment
+            packages = [
+              beamPackages.erlang
+              elixir
+            ];
           };
         };
       };
