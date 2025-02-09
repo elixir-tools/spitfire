3f935921f05d833778f14427cfe582ff40b5d224
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-02-09 16:40:39 -0500

    chore(nix): add act to flake
    Allows you to run the GHA locally with `act pull_request`

--- flake.nix
+++ flake.nix
@@ -10,20 +10,14 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [];
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: {
+      perSystem = {pkgs, ...}: {
         devShells = {
           default = pkgs.mkShell {
             # The Nix packages provided in the environment
             packages = with pkgs; [
               beam.packages.erlang_27.erlang
               beam.packages.erlang_27.elixir_1_17
+              act
             ];
           };
         };
